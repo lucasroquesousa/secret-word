eefb5787c986cc6bd914d0e87b449b8100582658
Lucas <lucasroquesousa@hotmail.com>
2022-11-27 21:10:28 -0300

    iniciando logica de programação para o jogo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import Header from "./components/Header";
 import StartScreen from "./components/StartScreen";
 import { GlobalStyle } from "./Styles/GlobalStyle";
 
+// import data
+import { wordList } from "../src/data/data";
+import { useState } from "react";
+
+const stage = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stage[0].name);
   return (
     <>
       <GlobalStyle />
       <Header />
-
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
     </>
   );
 }
